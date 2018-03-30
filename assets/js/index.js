e015b8be76c474430ec56b303a0326382cdad98d
raphael gilson <raphaelgilson7@gmail.com>
2018-03-30 19:27:24 +0200

    fixed bug 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,16 +2,4 @@ $(document).ready(function(){
   AOS.init({
     once: true,
   });
-  $('.my-paroller').paroller();
-  $("#matte").click(function () {
-    $("#select-type").val("matte");
-  });
-  $("#jet").click(function () {
-    $("#select-type").val("jet");
-  });
-  $("#down-button").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".first-section").offset().top
-    }, 750);
-  });
 });
