64ea3195aa2b208629cc9e393bd331828b3248f3
raphael gilson <raphaelgilson7@gmail.com>
2018-04-01 19:53:38 +0200

    commented dropdown

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   AOS.init({
     once: true,
   });
-  if($(ul.languagepicker li).hasClass('english')){
+  /* if($(ul.languagepicker li).hasClass('english')){
     $(".languagepicker").find(".lang-flag").addClass("english").removeClass("french");
   } else {
     $(".languagepicker").find(".lang-flag").addClass("french").removeClass("english");
-  }
+  }; */
 });
