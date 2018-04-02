80b9b8508b48496fd39ded6ab71cef0988ab7186
raphael gilson <raphaelgilson7@gmail.com>
2018-04-02 10:58:31 +0200

    language and modif done

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,4 @@ $(document).ready(function(){
   AOS.init({
     once: true,
   });
-  /* if($(ul.languagepicker li).hasClass('english')){
-    $(".languagepicker").find(".lang-flag").addClass("english").removeClass("french");
-  } else {
-    $(".languagepicker").find(".lang-flag").addClass("french").removeClass("english");
-  }; */
 });
